5c1769b11f76d7c05b664d0dc391a97a0e595f39
marie-p <mpbwelle@gmail.com>
2022-03-09 22:25:49 +0100

    correction

--- connect.php
+++ connect.php
@@ -1,19 +1,12 @@
 <?php
   try {
-    $port="8889";
+    $port="3306";
     $db="tp1_pagination_MariePaule";
     $user='root';
-    $pass='root';
+    $pass='my-secret-pw';
     $connect = "mysql:host=localhost:$port;dbname=$db";
     $database = new PDO($connect, $user, $pass);
-    // $result = $database->query('SELECT identifiant FROM personnes')->fetchAll();
-    // $identifiants = array();
-    // foreach($result as $value){
-    //   array_push($identifiants, $value[0]);
-    // }
-   
   } catch (PDOException $e) {
       print "Erreur !: " . $e->getMessage() . "<br/>";
-      die();
   }
 ?>
